9e4979419c22d8a17a912224188086ed2c9c84fe
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2025-07-05 13:30:22 +0530

    conditions in python

---
+++ python_condtions.py
@@ -0,0 +1,69 @@
+# Conditional statements are used to make decisions in your Python program based on conditions.
+
+# # 🔹 1. if Statement
+# x = 10
+
+# if x > 5:
+#     print("x is greate than 5")
+
+# 🔹 2. if-else Statement
+# x = 2
+# if x % 2 == 0:
+#     print("even number")
+# else:
+#     print("Odd nnumber")    
+
+# 🔹 3. if-elif-else Ladder
+
+# marks = 90;
+
+# if marks >= 90:
+#     print("grade A")
+# elif marks >= 75:
+#     print("Grade b")  
+# elif marks >= 60:
+#     print("grade c")   
+# else:
+#     print("fail")       
+
+# ex 2 
+# day = 7 ;
+# if day ==  1:
+#     print("manday")
+# elif day == 2:
+#     print("tuesday")   
+# elif day == 3:
+#     print("wendsday")  
+# elif day  == 4:
+#     print("thursday")   
+# elif day == 5:
+#     print("friday")  
+# elif day == 6:
+#     print("saturday")          
+# else:
+#     print("sunday")    
+
+# # 🔹 4. Nested if Statements
+# age = 18
+# if age >= 18:
+#     print("Adult")
+#     if age >= 60:
+#         print("Senior Citizen")
+# else:
+#     print("Minor")
+
+# 🔹 5. Short-Hand if (One-liner)
+# x = 7
+# if x > 5: print("x is big")
+
+# # 🔹 6. Short-Hand if-else (Ternary Operator)
+# a = 10
+# b = 20
+# print("a is greater") if a > b else print("b is greater")
+
+# # 🔹 Logical Operators with Conditions
+
+
+# x = 15
+# if x > 10 and x < 20:
+#     print("x is between 10 and 20")
